c62fd260b202a2cd07519c777cb0b1d198e948b1
Yabuinu98 <k4uk4u@naver.com>
2023-06-13 17:46:41 +0900

    ppl

--- soloplay/js/testpre_list.js
+++ soloplay/js/testpre_list.js
@@ -1,28 +1,24 @@
 document.addEventListener("DOMContentLoaded", () => {
-    const TableArea = document.querySelector(".Table_Area")
+    const Content = document.getElementById("content")
 
-    /* localstorage를 바디에 뿌려주기 */
+    /* localstorage를 받아오기 */
     let array = JSON.parse(localStorage.getItem("input"))
-    console.log(array)
-    console.log(array[0])
-    console.log(array[1])
-    console.log(array[1].순번)
-    console.log(array[1].제목)
-
-    const table = document.querySelector("table")
-
+    
+    /* localstorage를 뿌려주기 */
     for(let i = 0; i < array.length; i++){
-        let tr = document.createElement("tr")
-        let td1 = document.createElement("td")
-        let td2 = document.createElement("td")
-        let td3 = document.createElement("td")
-        let td4 = document.createElement("td")
-        let td5 = document.createElement("td")
-        table.appendChild(tr).append(td1,td2,td3,td4,td5)
-        td1.textContent = i+1
-        td2.textContent = array[i].제목
-        td3.textContent = array[i].작성자
-        td4.textContent = 999
-        td5.textContent = array[i].작성일자
+        /* div5개 생성하는걸 for문으로 돌리기 */
+        for(let j = 1; j<=5; j++){
+            const div = document.createElement("div")
+            div.setAttribute("id", "incontent" + j)
+            // const incontentj = document.getElementById("incontent"+j)
+        }
+
+        /* 생성된 div5개에 */
+        // Content.append(div)
+        // td1.textContent = i+1
+        // td2.textContent = array[i].제목
+        // td3.textContent = array[i].작성자
+        // td4.textContent = 999
+        // td5.textContent = array[i].작성일자
     }
 })
--- soloplay/js/testpre_write.js
+++ soloplay/js/testpre_write.js
@@ -1,15 +1,16 @@
 document.addEventListener("DOMContentLoaded", () => {
-    const titleInput = document.getElementById("title_input")
-    const titleText = document.getElementById("title_text")
-    const titleName = document.getElementById("title_name")
-    const 등록버튼 = document.getElementById("update")
+    const Title = document.getElementById("title")
+    const Name = document.getElementById("name")
+    const 작성버튼 = document.getElementById("summit")
 
     /* 1. arr에 localStroage input속성에 값이 있으면 빈배열 출력 */
+    
     // 아래 구문에서 푸쉬할 예정 => 제일 상단에 있지만 제일 마지막 느낌임
     let arr = JSON.parse(localStorage.getItem('input')) ?? []
 
-    /* 등록버튼을 누르면 -> localStroage에 저장되게하는 부분 */
-    등록버튼.addEventListener('click', () => {
+    /* 작성버튼을 누르면 -> localStroage에 저장되게하는 부분 */
+    작성버튼.addEventListener('click', () => {
+      
       /* 날짜를 받아오기 위해서 */
       /* new Date() 사용 => 'today.get~()'를 사용하기 위해 */
       let today = new Date();
@@ -18,33 +19,25 @@ document.addEventListener("DOMContentLoaded", () => {
       let date = today.getDate();
 
       /* 값을 입력 하지 않으면 alert로 경고문구 띄우기 */
-      if (titleInput.value == "") {
+      if (Title.value == "") {
         alert('제목을 입력해주세요.');
-        titleInput.focus();
-      } else if (titleText.value == "") {
-        alert('내용을 입력해주세요.');
-        titleText.focus()
-      } else if (titleName.value == "") {
+        Title.focus();
+      } else if (Name.value == "") {
         alert('작성자를 입력해주세요.');
-        titleName.focus();
+        Name.focus();
       } else {
         /* 1-1. arr배열에 객체 형식으로 밀어주기 */
         arr.push({ 
-          제목: titleInput.value, 
-          내용: titleText.value, 
-          작성자: titleName.value, 
+          제목: Title.value, 
+          작성자: Name.value, 
           작성일자: year + '/' + month + '/' + date })
 
           /* 1-2. 앞서 push받은 자료들을 localStorage input속성에 저장 */
           localStorage.setItem('input', JSON.stringify(arr))
   
         /* 1-3. 저장 후 input, textarea태그 내용 비우기(잔존 데이터 제거) */
-        titleInput.value = '';
-        titleText.value = '';
-        titleName.value = '';
+        Title.value = '';
+        Name.value = '';
       }
     })
-
-
-
   })
