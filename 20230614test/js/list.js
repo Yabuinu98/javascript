d47d4def8ca64f3bea4abcf3730aab0385ab8063
Yabuinu98 <k4uk4u@naver.com>
2023-06-14 16:00:38 +0900

    ppl

--- 20230614test/js/list.js
+++ 20230614test/js/list.js
@@ -1,29 +1,27 @@
 document.addEventListener("DOMContentLoaded", () => {
-    const Content = document.getElementById("content")
+    const Main = document.querySelector("main")
+    const clearbtn = document.getElementById("clear")
 
-    /* localstorage를 받아오기 */
-    let array = JSON.parse(localStorage.getItem("input"))
+    let Array = JSON.parse(localStorage.getItem("input"))
     
-    /* localstorage를 뿌려주기 */
-    for(let i = 0; i < array.length; i++){
+    for(let i = 0; i < Array.length; i++){
         let div = document.createElement("div")
         let span1 = document.createElement("span")
         const a = document.createElement("a")
         let span2 = document.createElement("span")
         let span3 = document.createElement("span")
         let span4 = document.createElement("span")
-        let span5 = document.createElement("span")
-        let span6 = document.createElement("span")
-        Content.appendChild(div).append(span1, a, span3, span4, span5, span6)
+        Main.appendChild(div).append(span1, a, span3, span4)
         a.appendChild(span2)
-        a.setAttribute("href", "./testpre_home.html")
-        // div.setAttribute("id", "content" + (i + 1))
-
+        a.setAttribute("href", "./write.html")
         span1.textContent = i+1
-        span2.textContent = array[i].제목
-        span3.textContent = array[i].작성자
-        span4.textContent = array[i].작성일자
-        span5.textContent = 999
-        span6.textContent = 999
+        span2.textContent = Array[i].제목
+        span3.textContent = Array[i].작성자
+        span4.textContent = Array[i].작성일자
     }
+
+    clearbtn.addEventListener("click", () => {
+        localStorage.clear()
+        location.reload()
+    })
 })
--- 20230614test/js/write.js
+++ 20230614test/js/write.js
@@ -4,23 +4,14 @@ document.addEventListener("DOMContentLoaded", () => {
     const Name = document.getElementById("name")
     const 작성버튼 = document.getElementById("summit")
 
-    /* 1. arr에 localStroage input속성에 값이 있으면 빈배열 출력 */
-    
-    // 아래 구문에서 푸쉬할 예정 => 제일 상단에 있지만 제일 마지막 느낌임
-    // ?? 연산자는 null 또는 undefined인 경우에 대체 값을 지정하는 연산자
     let arr = JSON.parse(localStorage.getItem('input')) ?? []
 
-    /* 작성버튼을 누르면 -> localStroage에 저장되게하는 부분 */
     작성버튼.addEventListener('click', () => {
-      
-      /* 날짜를 받아오기 위해서 */
-      /* new Date() 사용 => 'today.get~()'를 사용하기 위해 */
       let today = new Date();
       let year = today.getFullYear();
-      let month = today.getMonth() + 1;  // 월(0~11까지 돌아서 +1)
+      let month = today.getMonth() + 1;
       let date = today.getDate();
 
-      /* 값을 입력 하지 않으면 alert로 경고문구 띄우기 */
       if (Title.value == "") {
         alert('제목을 입력해주세요.');
         Title.focus();
@@ -28,19 +19,15 @@ document.addEventListener("DOMContentLoaded", () => {
         alert('작성자를 입력해주세요.');
         Name.focus();
       } else {
-        /* 1-1. arr배열에 객체 형식으로 밀어주기 */
         arr.push({ 
           제목: Title.value, 
+          내용: Discription.value,
           작성자: Name.value, 
           작성일자: year + '/' + month + '/' + date })
-
-          /* 1-2. 앞서 push받은 자료들을 localStorage input속성에 저장 */
           localStorage.setItem('input', JSON.stringify(arr))
-  
-        /* 1-3. 저장 후 input, textarea태그 내용 비우기(잔존 데이터 제거) */
-        Title.value = '';
-        Discription.value = '';
-        Name.value = '';
+          Title.value = '';
+          Discription.value = '';
+          Name.value = '';
       }
     })
   })
